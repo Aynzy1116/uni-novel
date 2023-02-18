da39586d52a5dedf86ea2855904dd263f6203477
xiaodu1116 <Aynzy1116@163.com>
2023-02-18 14:41:49 +0800

    # 测试

--- pages/personal/personal.vue
+++ pages/personal/personal.vue
@@ -1,8 +1,23 @@
 <template>
-	<view>个人</view>
+	<view>
+		<view>个人</view>
+		<button @getphonenumber="getPhoneNumber" open-type="getPhoneNumber" >授权登入</button>
+	</view>
 </template>
 
 <script>
+	export default{
+		data(){
+			return{
+				
+			}
+		},
+		methods:{
+			getPhoneNumber(e){
+				console.log('e',e);
+			}
+		}
+	}
 </script>
 
 <style>
